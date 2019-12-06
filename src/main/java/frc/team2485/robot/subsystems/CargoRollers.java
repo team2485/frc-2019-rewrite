cf0edd6287a6a93b5fedb8c859b9cb1b0ae0da10
mark-rifkin <mrifkin2022@francisparker.org>
2019-12-06 12:34:59 -0800

    Created cargo rollers subsystem

---
+++ src/main/java/frc/team2485/robot/subsystems/CargoRollers.java
@@ -0,0 +1,29 @@
+package frc.team2485.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.team2485.robot.Constants.CargoRollersConstants.CARGO_ROLLERS_MOTOR_PORT;
+
+public class CargoRollers extends SubsystemBase {
+    private final WPI_TalonSRX cargoRollersTalon = new WPI_TalonSRX(CARGO_ROLLERS_MOTOR_PORT);
+
+    private boolean intaken;
+
+    public CargoRollers() {
+        intaken = false;
+    }
+
+    public void setRollersManual(double power) {
+        cargoRollersTalon.set(ControlMode.PercentOutput, power);
+    }
+
+    public void setIntaken(boolean intaken) {
+        this.intaken = intaken;
+    }
+
+    public boolean getIntaken() {
+        return this.intaken;
+    }
+}
