ad706edba06164c2ab627538d9a8248cbb035597
Nathan Sariowan <ncsariowan@gmail.com>
2019-12-10 19:15:01 -0800

    Update cargoRollers to WarlordsLib

--- src/main/java/frc/team2485/robot/Constants.java
+++ src/main/java/frc/team2485/robot/Constants.java
@@ -2,29 +2,6 @@ package frc.team2485.robot;
 
 public class Constants {
 
-    /**
-     * WPILib Button enum is public in next release; private currently
-     */
-    public enum Button {
-        kBumperLeft(5),
-        kBumperRight(6),
-        kStickLeft(9),
-        kStickRight(10),
-        kA(1),
-        kB(2),
-        kX(3),
-        kY(4),
-        kBack(7),
-        kStart(8);
-
-        @SuppressWarnings({"MemberName", "PMD.SingularField"})
-        public final int value;
-
-        Button(int value) {
-            this.value = value;
-        }
-    }
-
 
     
 }
--- src/main/java/frc/team2485/robot/RobotContainer.java
+++ src/main/java/frc/team2485/robot/RobotContainer.java
@@ -7,20 +7,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.team2485.WarlordsLib.commands.IfCommand;
-import frc.team2485.WarlordsLib.robotConfigs.RobotConfigurator;
+import frc.team2485.WarlordsLib.oi.WL_XboxController;
+import frc.team2485.WarlordsLib.oi.WL_XboxController.XboxButton;
+import frc.team2485.WarlordsLib.oi.WL_XboxController.XboxJoystick;
 import frc.team2485.robot.commands.SetDrivetrainAngle;
 import frc.team2485.robot.subsystems.CargoRollers;
 import frc.team2485.robot.subsystems.Drivetrain;
 import frc.team2485.robot.subsystems.HatchIntake;
 import frc.team2485.robot.subsystems.HatchIntakeRollers;
 
-import java.time.Instant;
-
 public class RobotContainer {
 
-    private XboxController jack;
-    private XboxController suraj;
+    private WL_XboxController jack;
+    private WL_XboxController suraj;
 
     private Drivetrain drivetrain;
     private HatchIntake hatchIntake;
@@ -34,8 +33,8 @@ public class RobotContainer {
         hatchIntakeRollers = new HatchIntakeRollers();
         cargoRollers = new CargoRollers();
 
-        jack = new XboxController(0);
-        suraj = new XboxController(1);
+        jack = new WL_XboxController(0);
+        suraj = new WL_XboxController(1);
 
         SmartDashboard.putData(drivetrain);
         SmartDashboard.putData(hatchIntake);
@@ -45,34 +44,28 @@ public class RobotContainer {
 
     public void configureCommands() {
 
+
+        Shuffleboard.getTab("Drivetrain").add("SetDrivetrainAngle", new SetDrivetrainAngle(drivetrain, Math.PI/2));
+
+
         drivetrain.setDefaultCommand(new RunCommand(() ->
             drivetrain.curvatureDrive(jack.getTriggerAxis(GenericHID.Hand.kRight) - jack.getTriggerAxis(GenericHID.Hand.kLeft), jack.getX(GenericHID.Hand.kLeft), jack.getXButton())
                 , drivetrain));
 
-        /*Hatch Controls
-        Jack left bumper lifts up intake and slides it out, ready for hatch intake.
-        Jack A sets rollers to intake when pressed and stops intaking when released.
-        Jack right bumper stows hatch intake.
-
-
-         */
-
-        new JoystickButton(jack, Constants.Button.kBumperLeft.value)
+        jack.getJoystickButton(XboxButton.kBumperLeft)
                 .whenPressed(new SequentialCommandGroup(
                         new InstantCommand(hatchIntake::lift),
                         new WaitCommand(0.2),
                         new InstantCommand(hatchIntake::slideOut)));
 
 
-        new JoystickButton(jack, Constants.Button.kBumperRight.value)
+        jack.getJoystickButton(XboxButton.kBumperRight)
                 .whenPressed(new InstantCommand(hatchIntake::slideIn)
                     .andThen(new InstantCommand(hatchIntake::stow)));
 
-        new JoystickButton(jack, Constants.Button.kA.value)
+        jack.getJoystickButton(XboxButton.kA)
                 .whenPressed(new InstantCommand(()->hatchIntakeRollers.setRollers(0.8)))
                 .whenReleased(new InstantCommand(hatchIntakeRollers::stopRollers));
-
-        Shuffleboard.getTab("Drivetrain").add("SetDrivetrainAngle", new SetDrivetrainAngle(drivetrain, Math.PI/2));
 
 //        new Trigger(() -> {
 //            return jack.getTriggerAxis(GenericHID.Hand.kRight) > 0.2;
@@ -85,7 +78,7 @@ public class RobotContainer {
         Suraj left trigger: press to outtake, release to stop
          */
         //Cargo intake
-        new JoystickButton(suraj, Constants.Button.kBumperLeft.value)
+        suraj.getJoystickButton(XboxButton.kBumperLeft)
                 .whenPressed(new SequentialCommandGroup(new InstantCommand(hatchIntake::slideIn),
                                                         new InstantCommand(hatchIntake::stow),
                                                         new InstantCommand(()->cargoRollers.setRollersManual(0.5)),
@@ -93,22 +86,12 @@ public class RobotContainer {
                                                         new ParallelRaceGroup(new WaitUntilCommand(()->!cargoRollers.isSpiking()), new WaitCommand(500)),
                                                         new ConditionalCommand(new InstantCommand(()->cargoRollers.setRollersManual(0.2)), null, cargoRollers::isSpiking)));
 
-
         //cargo outake
-        new Trigger(() -> {
-            return suraj.getTriggerAxis(GenericHID.Hand.kLeft) > 0.2;
-        }).whenActive(new InstantCommand(() -> {
-            cargoRollers.setRollersManual(-0.4);
-        }));
+        suraj.getJoystickAxisButton(XboxJoystick.kTriggerRight, 0.2)
+                .whenActive(new InstantCommand(() -> cargoRollers.setRollersManual(-0.4)));
 
-        new Trigger(() -> {
-            return suraj.getTriggerAxis(GenericHID.Hand.kLeft) > 0.2;
-        }).whenInactive(new InstantCommand(() -> {
-            cargoRollers.setRollersManual(0);
-        }));
-
-
-
+        suraj.getJoystickAxisButton(XboxJoystick.kTriggerLeft, 0.2)
+                .whenInactive(new InstantCommand(() -> cargoRollers.setRollersManual(0)));
     }
 
     public Command getAutonomousCommand() {
